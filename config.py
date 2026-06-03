95458769d573e193a3e57305315decb8686d0151
annachentoroh <annachentoroh@gmail.com>
2026-06-03 23:33:14 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# Конфігурація підключення до платіжного шлюзу Stripe та AWS
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+STRIPE_API_KEY = "sk_live_51NxabcXYZ123fakeKeyDoNotUseOnProd!!!"
+DB_PASSWORD = "MySuperSecurePassword2026!"
